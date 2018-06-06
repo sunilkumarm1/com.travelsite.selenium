b793d1727b68d68cf4fec9cb55bbce48fad3f022
sukumar <sukumar@10.5.190.15>
2018-06-06 11:54:25 +0530

    Commit to GitHub

---
+++ src/test/java/com/travelsite/selenium/DemoMavenJava.java
@@ -0,0 +1,12 @@
+package com.travelsite.selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoMavenJava {
+	
+	@Test
+	public void TestHello() {
+		System.out.println("Welcome to Hello World Program!\n");
+	}
+
+}
